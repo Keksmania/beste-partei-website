8752fac0c78d8c78c22254ca69804dc5501006b1
Alper <alper199312@yahoo.de>
2024-11-21 21:19:59 +0000

    Initial commit

---
+++ resources/views/start.blade.php
@@ -0,0 +1,8 @@
+<div>
+    	
+	<h1> HELLO WORLD! </h1>
+	<p>Hallo meine lieben Parteimitglieder</p>
+	<p>Hiermit Grüße ich offiziell Paul.</p> 
+	<p>Ihr Server-Admin </p>
+	<p> Dies ist eine Laravel-Webseite die auf ngix läuft O: </p>
+</div>
