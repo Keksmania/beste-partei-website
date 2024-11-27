4f9255dd743b2d8f2336b6b6ab5148ce13e0aa9a
Alper <alper199312@yahoo.de>
2024-11-27 10:26:24 +0000

    vue wurde installiert und funktioniert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import test from './components/test.vue';
+const app = createApp({});
+app.component('test', test);
+app.mount("#app");
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,8 +1,32 @@
-<div>
-    	
-	<h1> HELLO WORLD! </h1>
-	<p>Hallo meine lieben Parteimitglieder</p>
-	<p>Hiermit Grüße ich offiziell Paul.</p> 
-	<p>Ihr Server-Admin </p>
-	<p> Dies ist eine Laravel-Webseite die auf ngix läuft O: </p>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100" id="app">
+	<div>
+
+        <h1> HELLO WORLD! </h1>
+        <p>Hallo meine lieben Parteimitglieder</p>
+        <p>Hiermit Grüße ich offiziell Paul.</p> 
+        <p>Ihr Server-Admin </p>
+        <p> Dies ist eine Laravel-Webseite die auf ngix läuft O: </p>
+	<test></test>
 </div>
+
+
+	</div> 
+</body>
+
+</html>
